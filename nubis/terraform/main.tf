c5e4598fe42f2a76382f6d7549185f71b75986b6
joeyk <jkrejci@mozilla.com>
2018-05-24 13:37:32 -0700

    removed SSL cert for testing

--- nubis/terraform/main.tf
+++ nubis/terraform/main.tf
@@ -22,7 +22,6 @@ module "load_balancer" {
   account              = "${var.account}"
   service_name         = "${var.service_name}"
   health_check_target  = "HTTP:80/health.php"
-  ssl_cert_name_prefix = "${var.service_name}"
 }
 
 module "database" {
