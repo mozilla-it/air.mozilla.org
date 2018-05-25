a510861156bda045226e6643d877642774305833
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-05-25 10:10:49 -0400

    Apache fixups:  - use nubis_apache for candy (exporter, discovery, etc)  - set default_vhost  - Use full paths to the WSGI scripts  - set python home to the holding directory (for settings files resolution)  - Put back most apache commented-out code

--- nubis/puppet/apache.pp
+++ nubis/puppet/apache.pp
@@ -1,11 +1,12 @@
 # Define how Apache should be installed and configured
-
+class { 'nubis_apache': }
 
 class { 'apache::mod::wsgi': }
 
 #WSGI Setup for AirmoFront
 apache::vhost { $project_name:
   serveradmin		      => 'webops@mozilla.com',
+  default_vhost               => true,
   port                        => 80,
   docroot                     => "/var/www/${project_name}",
   wsgi_application_group      => '%{GLOBAL}',
@@ -14,31 +15,31 @@ apache::vhost { $project_name:
     processes    => '2',
     threads      => '15',
     display-name => '%{GROUP}',
+    home         => "/var/www/${project_name}",
   },
-  wsgi_import_script          => 'airmozilla/wsgi.py',
+  wsgi_import_script          => '/var/www/${project_name}/airmozilla/wsgi.py',
   wsgi_import_script_options  => {
     process-group     => 'wsgi',
     application-group => '%{GLOBAL}',
   },
   wsgi_process_group          => 'wsgi',
-  wsgi_script_aliases         => { '/' => 'airmozilla/wsgi.py' },
+  wsgi_script_aliases         => { '/' => '/var/www/${project_name}/airmozilla/wsgi.py' },
 
+    custom_fragment    => "
+        # Don't set default expiry on anything
+        ExpiresActive Off
 
-#    custom_fragment    => "
-#        # Don't set default expiry on anything
-#        ExpiresActive Off
-#
-#        # Clustered without coordination
-#        FileETag None
-#
-#	# Mark internal traffic as not log-worthy
-#	SetEnvIfExpr \"-R '10.0.0.0/8' || -R '172.16.0.0/12' || -R '192.168.0.0/16' || -R '127.0.0.0/8'\" internal
-#    " 
-#   
-#    block              => ['scm'],
-#    setenvif           => [
-#      'X-Forwarded-Proto https HTTPS=on',
-#    ],
+        # Clustered without coordination
+        FileETag None
+
+	# Mark internal traffic as not log-worthy
+	SetEnvIfExpr \"-R '10.0.0.0/8' || -R '172.16.0.0/12' || -R '192.168.0.0/16' || -R '127.0.0.0/8'\" internal
+    "
+
+    block              => ['scm'],
+    setenvif           => [
+      'X-Forwarded-Proto https HTTPS=on',
+    ],
     access_log_env_var => '!internal',
     access_log_format  => '%a %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-agent}i\"',
     headers            => [
