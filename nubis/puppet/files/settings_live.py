a89580363a97262c6b318ee27013b0f9c3c9586d
limed <limed@mozilla.com>
2018-05-24 19:21:02 -0700

    Add missing file

---
+++ nubis/puppet/files/settings_live.py
@@ -0,0 +1,25 @@
+import os
+
+from .settings import *
+
+import imp
+nubis = imp.load_source('nubis', '/etc/nubis-config/airmofront.sh')
+
+DEBUG = False
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': nubis.Cache_Endpoint + ':' + nubis.Cache_Port,
+        'KEY_PREFIX': 'airmozilla-live',
+    }
+}
+
+ALLOWED_HOSTS = ['air.mozilla.org']
+
+# Mozilla is deploying this, and db backups aren't necesary since it's
+# generated on a schedule.
+# BACKUPDB_DIRECTORY = os.environ['BACKUP_DIR']
+STATIC_ROOT = os.environ['STATIC_ROOT']
+SECRET_KEY = os.environ['SECRET_KEY']
+COMPRESS_ENABLED = True
