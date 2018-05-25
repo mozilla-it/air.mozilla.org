5fe91a83c353af7c67653709b1fb20d1009920e3
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-05-25 10:16:00 -0400

    puppet lint

--- nubis/puppet/apache.pp
+++ nubis/puppet/apache.pp
@@ -5,7 +5,7 @@ class { 'apache::mod::wsgi': }
 
 #WSGI Setup for AirmoFront
 apache::vhost { $project_name:
-  serveradmin		      => 'webops@mozilla.com',
+  serveradmin                 => 'webops@mozilla.com',
   default_vhost               => true,
   port                        => 80,
   docroot                     => "/var/www/${project_name}",
@@ -17,15 +17,15 @@ apache::vhost { $project_name:
     display-name => '%{GROUP}',
     home         => "/var/www/${project_name}",
   },
-  wsgi_import_script          => '/var/www/${project_name}/airmozilla/wsgi.py',
+  wsgi_import_script          => "/var/www/${project_name}/airmozilla/wsgi.py",
   wsgi_import_script_options  => {
     process-group     => 'wsgi',
     application-group => '%{GLOBAL}',
   },
   wsgi_process_group          => 'wsgi',
-  wsgi_script_aliases         => { '/' => '/var/www/${project_name}/airmozilla/wsgi.py' },
+  wsgi_script_aliases         => { '/' => "/var/www/${project_name}/airmozilla/wsgi.py" },
 
-    custom_fragment    => "
+  custom_fragment             => "
         # Don't set default expiry on anything
         ExpiresActive Off
 
@@ -36,22 +36,22 @@ apache::vhost { $project_name:
 	SetEnvIfExpr \"-R '10.0.0.0/8' || -R '172.16.0.0/12' || -R '192.168.0.0/16' || -R '127.0.0.0/8'\" internal
     "
 
-    block              => ['scm'],
-    setenvif           => [
-      'X-Forwarded-Proto https HTTPS=on',
-    ],
-    access_log_env_var => '!internal',
-    access_log_format  => '%a %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-agent}i\"',
-    headers            => [
-      "set X-Nubis-Version ${project_version}",
-      "set X-Nubis-Project ${project_name}",
-      "set X-Nubis-Build   ${packer_build_name}",
-    ],
-    rewrites           => [
-      {
-        comment      => 'HTTPS redirect',
-        rewrite_cond => ['%{HTTP:X-Forwarded-Proto} =http'],
-        rewrite_rule => ['. https://%{HTTP:Host}%{REQUEST_URI} [L,R=permanent]'],
-      }
-    ]
+  block                       => ['scm'],
+  setenvif                    => [
+    'X-Forwarded-Proto https HTTPS=on',
+  ],
+  access_log_env_var          => '!internal',
+  access_log_format           => '%a %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-agent}i\"',
+  headers                     => [
+    "set X-Nubis-Version ${project_version}",
+    "set X-Nubis-Project ${project_name}",
+    "set X-Nubis-Build   ${packer_build_name}",
+  ],
+  rewrites                    => [
+    {
+      comment      => 'HTTPS redirect',
+      rewrite_cond => ['%{HTTP:X-Forwarded-Proto} =http'],
+      rewrite_rule => ['. https://%{HTTP:Host}%{REQUEST_URI} [L,R=permanent]'],
+    }
+  ]
 }
