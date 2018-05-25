9fddcca407e1d79a2666dbedcaefe75dd065c5fd
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-05-25 14:38:47 -0400

    use the airmozilla/settings_live.py settings file

--- nubis/puppet/apache.pp
+++ nubis/puppet/apache.pp
@@ -39,7 +39,9 @@ apache::vhost { $project_name:
     # Mark internal traffic as not log-worthy
     SetEnvIfExpr \"-R '10.0.0.0/8' || -R '172.16.0.0/12' || -R '192.168.0.0/16' || -R '127.0.0.0/8'\" internal
   ",
-
+  setenv => [
+    'DJANGO_SETTINGS_MODULE airmozilla.settings_live',
+  ],
   aliases                     => [
     {
       alias => '/static',
