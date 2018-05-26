47de2489b44c1cc0d9a17f333ac23d1c35c7b5be
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-05-26 17:53:48 -0400

    Make sure to keep using outbound proxies even when computing redirects

--- nubis/puppet/files/wsgi.py
+++ nubis/puppet/files/wsgi.py
@@ -13,4 +13,11 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "airmozilla.settings_nubis")
 
+# Set Outbound Proxies
+
+os.environ["http_proxy"]="http://proxy.service.consul:3128/"
+os.environ["HTTPS_PROXY"]="http://proxy.service.consul:3128/"
+os.environ["https_proxy"]="http://proxy.service.consul:3128/"
+os.environ["HTTP_PROXY"]="http://proxy.service.consul:3128/"
+
 application = get_wsgi_application()
