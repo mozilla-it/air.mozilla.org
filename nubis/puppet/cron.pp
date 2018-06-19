cc8b24f270bd2d7d1b71309d25eb7cd514bdd1ba
joeyk <jkrejci@mozilla.com>
2018-06-19 11:04:18 -0700

    fixed venv on the manage.py command

--- nubis/puppet/cron.pp
+++ nubis/puppet/cron.pp
@@ -1,4 +1,4 @@
-$manage_command =  "nubis-cron ${project_name}-manage /var/www/airmofront/manage.py refresh_events"
+manage_command = "nubis-cron ${project_name}-manage /opt/${projectname}/venv/bin/python /var/www/${project_name}/manage.py refresh_events"
 
 #Run manage.py every hour
 cron::hourly { "${project_name}-manage":
