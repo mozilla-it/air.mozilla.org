f5704e257ac6f506a99d6234fcc7837d6b204f75
joeyk <jkrejci@mozilla.com>
2018-06-25 16:33:54 -0700

    Debug mode test

--- airmozilla/settings.py
+++ airmozilla/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'insecure sekrit key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
