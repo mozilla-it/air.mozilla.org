b6eb5637b51c443ab02d8dad2ae041cab1aa80cc
joeyk <jkrejci@mozilla.com>
2018-06-26 14:32:03 -0700

    Added rewrite table and apache rewrites

--- nubis/puppet/airmofront.pp
+++ nubis/puppet/airmofront.pp
@@ -29,12 +29,12 @@ python::requirements { "/var/www/${project_name}/requirements.txt" :
 
 file { "/var/www/${project_name}/airmozilla/settings_nubis.py":
   ensure => present,
-  source => 'puppet:///nubis/files/settings.py'
+  source => 'puppet:///modules/nubis/files/settings.py'
 }
 
 file { "/var/www/${project_name}/airmozilla/wsgi_nubis.py":
   ensure => present,
-  source => 'puppet:///nubis/files/wsgi.py'
+  source => 'puppet:///modules/nubis/files/wsgi.py'
 }
 
 file { "/var/www/${project_name}/static/CACHE":
@@ -68,7 +68,7 @@ DJANGO_SETTINGS_MODULE=airmozilla.settings_nubis
 
 file { "/usr/local/bin/${project_name}-update":
   ensure => present,
-  source => 'puppet:///nubis/files/update',
+  source => 'puppet:///modules/nubis/files/update',
   owner  => root,
   group  => root,
   mode   => '0755',
@@ -81,3 +81,13 @@ nubis::configuration { $project_name:
   reload => "/usr/local/bin/${project_name}-update"
 }
 
+file { '/etc/apache2/airmolegacyurlsmap.txt':
+  ensure  => present,
+  source  => 'puppet:///modules/nubis/files/airmolegacyurlsmap.txt',
+  owner   => root,
+  group   => root,
+  mode    => '0644',
+  require => [
+    Class['apache'],
+  ],
+}
