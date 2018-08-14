2c36668c0adca98558e70528266ce144f08d47c3
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-08-14 13:50:51 -0400

    disable cron

--- nubis/puppet/cron.pp
+++ nubis/puppet/cron.pp
@@ -1,7 +1,11 @@
 $manage_command = "/usr/local/bin/consul-do $(nubis-metadata NUBIS_PROJECT)-$(nubis-metadata NUBIS_ENVIRONMENT)-manage $(hostname) && nubis-cron ${project_name}-manage /opt/${project_name}/venv/bin/python /var/www/${project_name}/manage.py refresh_events"
 
-#Run manage.py every hour
-cron::hourly { "${project_name}-manage":
-  user    => $apache::params::user,
-  command => $manage_command,
+$airmo_cron_enabled = false
+
+if $airmo_cron_enabled {
+  #Run manage.py every hour
+  cron::hourly { "${project_name}-manage":
+    user    => $apache::params::user,
+    command => $manage_command,
+  }
 }
