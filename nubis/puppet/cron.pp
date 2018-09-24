0c085479a19fd8c0d7cfe8caf7c862adf3de4e2b
Daniel Hartnell <danielhartnell@gmail.com>
2018-09-24 07:35:08 -0700

    re-enabling the cron job

--- nubis/puppet/cron.pp
+++ nubis/puppet/cron.pp
@@ -1,6 +1,6 @@
 $manage_command = "/usr/local/bin/consul-do $(nubis-metadata NUBIS_PROJECT)-$(nubis-metadata NUBIS_ENVIRONMENT)-manage $(hostname) && nubis-cron ${project_name}-manage /opt/${project_name}/venv/bin/python /var/www/${project_name}/manage.py refresh_events"
 
-$airmo_cron_enabled = false
+$airmo_cron_enabled = true
 
 if $airmo_cron_enabled {
   #Run manage.py every hour
