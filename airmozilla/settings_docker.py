ebe9880c08a0fa1c4f20f81c143050f2529d33d5
Philippe M. Chiasson <gozer@ectoplasm.org>
2019-09-25 13:10:16 -0400

    [k8s] Use environemnt for DB config

--- airmozilla/settings_docker.py
+++ airmozilla/settings_docker.py
@@ -9,6 +9,16 @@ ALLOWED_HOSTS = [ "*" ]
 
 #SECRET_KEY = ""
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER':  os.getenv('DATABASE_USERNAME'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST'),
+    }
+}
+
 COMPRESS_ENABLED = True
 COMPRESS_OFFILNE = True
 
