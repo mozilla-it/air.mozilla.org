92bf07da49ef9c24da4239077f258d133ac79c6a
Philippe M. Chiasson <gozer@ectoplasm.org>
2019-11-01 21:50:49 -0400

    add newrelic

--- docker/wsgi.py
+++ docker/wsgi.py
@@ -7,6 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
+import newrelic.agent
+newrelic.agent.initialize()
+
 import os
 
 from django.core.wsgi import get_wsgi_application
